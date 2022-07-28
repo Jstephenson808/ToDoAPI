71f9fa5a6efd3d35fbf18eb9611f7fd647d17035
James Stephenson <jstephenson808@gmail.com>
2022-07-28 11:28:44 +0100

    add a full suite of integration tests

--- src/test/java/com/verint/todoapi/ToDoIntegrationTest.java
+++ src/test/java/com/verint/todoapi/ToDoIntegrationTest.java
@@ -6,11 +6,10 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.w3c.dom.CDATASection;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 
@@ -19,7 +18,6 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ToDoIntegrationTest {
 
-    //ToDo this will be the issue when in prod env
     private final static String BASE_URI = "http://localhost";
 
     @LocalServerPort
@@ -32,20 +30,63 @@ public class ToDoIntegrationTest {
     }
 
     @Test
-    void postTest(){
-        given().port(port).contentType(ContentType.JSON).body("""
-                                                        {"name":"James S"}
-                                                        """)
+    void postRequest_ReturnCreatedToDo(){
+        given()
+                .port(port).contentType(ContentType.JSON)
+                .body("""
+                      {"name":"Post Test"}
+                      """)
                 .when().post("/todos")
                 .then().assertThat().statusCode(200)
                 .body("id", equalTo(1))
-                .body("name", equalTo("James S"));
+                .body("name", equalTo("Post Test"));
     }
 
     @Test
-    void exampleTest() {
-        given().port(port).when().get("/todos")
-                .then().assertThat().statusCode(200);
+    void getRequest_ReturnToDos() {
+        given()
+                .port(port).contentType(ContentType.JSON)
+                .body("""
+                      {"name":"Get Test"}
+                      """)
+                .when().post("/todos");
+
+        given()
+                .port(port)
+                .when().get("/todos")
+                .then().assertThat().statusCode(200)
+                .body("id", hasItem(1))
+                .body("name", hasItem("Get Test"));
+    }
+
+    @Test
+    void delete_ToDoExists_SuccessCode(){
+        given()
+                .port(port).contentType(ContentType.JSON)
+                .body("""
+                      {"name":"Get Test"}
+                      """)
+                .when().post("/todos");
+        given()
+                .port(port)
+                .when().delete("/todos/1")
+                .then().assertThat().statusCode(204);
+    }
+
+    @Test
+    void delete_ToDoDoesNotExist_NotFoundCode(){
+        given()
+                .port(port)
+                .when().delete("/todos/999")
+                .then().assertThat().statusCode(404);
+    }
+
+    @Test
+    void post_noBody_UnsupportedMediaError(){
+        given()
+                .port(port).when()
+                .post("todos")
+                .then().assertThat().statusCode(415);
     }
 }
 
