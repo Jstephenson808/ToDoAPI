f8d644d240968b73cdd97e8ba675c2350ed8624f
James Stephenson <jstephenson808@gmail.com>
2022-08-02 13:37:14 +0100

    fix test not recevied error and tidy up tests to use dynamic db ids

--- src/integration-test/java/com/verint/todoapi/ToDoIntegrationTest.java
+++ src/integration-test/java/com/verint/todoapi/ToDoIntegrationTest.java
@@ -3,26 +3,40 @@ package com.verint.todoapi;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class ToDoIntegrationTest {
 
+    private static Integer nextId;
+    private static Integer startId;
+
     @BeforeAll
-    public void config(){
+    public static void config(){
         if(System.getenv("url") == null){
             RestAssured.baseURI = "http://localhost:8080";
         }
         else{
             RestAssured.baseURI = System.getenv("url");
         }
-
+        startId = given()
+                                .contentType(ContentType.JSON)
+                                    .body("""
+                                          {"name":"Next ID"}
+                                          """)
+                                .when()
+                                    .post("/todos")
+                                .then()
+                                    .extract().path("id");
+        nextId = startId + 1;
     }
 
     @Test
+    @Order(1)
     void postRequest_ReturnCreatedToDo(){
         given()
                 .contentType(ContentType.JSON)
@@ -31,37 +45,32 @@ public class ToDoIntegrationTest {
                       """)
                 .when().post("/todos")
                 .then().assertThat().statusCode(200)
-                .body("id", Matchers.equalTo(1))
+                .body("id", Matchers.equalTo(nextId))
                 .body("name", Matchers.equalTo("Post Test"));
     }
 
     @Test
+    @Order(2)
     void getRequest_ReturnToDos() {
-        given()
-                .contentType(ContentType.JSON)
-                .body("""
-                      {"name":"Get Test"}
-                      """)
-                .when().post("/todos");
-
         given()
                 .when().get("/todos")
                 .then().assertThat().statusCode(200)
-                .body("id", Matchers.hasItem(1))
-                .body("name", Matchers.hasItem("Get Test"));
+                .body("id", Matchers.hasItem(nextId))
+                .body("name", Matchers.hasItem("Post Test"));
     }
 
     @Test
+    @Order(3)
     void delete_ToDoExists_SuccessCode(){
         given()
-                .contentType(ContentType.JSON)
-                .body("""
-                      {"name":"Get Test"}
-                      """)
-                .when().post("/todos");
-        given()
-                .when().delete("/todos/1")
+                .when().delete("/todos/" + nextId.toString())
                 .then().assertThat().statusCode(204);
+    }
+
+    @AfterAll
+    public static void cleanUp(){
+        given()
+                .when().delete("/todos/" + startId.toString());
     }
 }
 
